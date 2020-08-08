e5632c293ac4477b95abf5f16bc3729cbe8e1835
ibtg <ibtg0409@gmail.com>
2020-08-08 14:35:35 +0900

    add feature days
    add feature to display how long user use this app.
    it is displayed in the upper right corner

---
+++ src/days.js
@@ -0,0 +1,47 @@
+const days = document.querySelector('.days');
+const DAYS_LS = 'days';
+
+const getDate = () => {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const dateString = `${year}-${month < 10 ? `0${month}` : month}-${
+    day > 0 ? `0${day}` : day
+  }`;
+  return dateString;
+};
+
+const saveDate = (date) => {
+  localStorage.setItem(DAYS_LS, date);
+};
+
+const paintDays = (day) => {
+  days.innerText = `${day} day`;
+};
+
+const daysInit = () => {
+  const currentDays = localStorage.getItem(DAYS_LS);
+  if (currentDays === null) {
+    paintDays(1);
+    const currentDate = getDate();
+    const dateJson = JSON.stringify({ startDate: currentDate, dayCount: 1 });
+    saveDate(dateJson);
+  } else {
+    const currentDate = getDate();
+    const startDate = JSON.parse(currentDays).startDate;
+    let dayCountValue = JSON.parse(currentDays).dayCount;
+    dayCountValue =
+      currentDate === startDate ? dayCountValue : dayCountValue + 1;
+
+    const dateJson = JSON.stringify({
+      startDate: currentDate,
+      dayCount: dayCountValue,
+    });
+
+    paintDays(dayCountValue);
+    saveDate(dateJson);
+  }
+};
+
+daysInit();
