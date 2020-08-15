7c5be338612461b7ca49a719270368df6241cc4c
ibtg <ibtg0409@gmail.com>
2020-08-15 15:08:25 +0900

    modify http to https in weather.js

--- src/weather.js
+++ src/weather.js
@@ -27,7 +27,7 @@ const paintWeather = (obj) => {
 
 const getWeather = (lat, lon) => {
   fetch(
-    `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
   )
     .then((response) => {
       return response.json();
