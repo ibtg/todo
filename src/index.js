2f8f4eedc21ae9911e44cef532411117fbeda18e
ibtg <ibtg0409@gmail.com>
2020-09-02 17:11:49 +0900

    Refactor quotes.js to class

--- src/index.js
+++ src/index.js
@@ -2,9 +2,24 @@
 
 import Clock from './clock.js';
 import Weather from './weather.js';
+import Greeting from './greeting.js';
+import Quotes from './quotes.js';
 
 const clock = new Clock();
 clock.StartTime();
 
 const weather = new Weather();
 weather.loadCoords();
+
+const greeting = new Greeting();
+greeting.loadName();
+
+greeting.userForm.addEventListener('submit', () => {
+  const quotes = new Quotes();
+  quotes.getQuotes();
+});
+
+if (localStorage.getItem(greeting.USER_LS)) {
+  const quotes = new Quotes();
+  quotes.getQuotes();
+}
--- src/quotes.js
+++ src/quotes.js
@@ -1,64 +1,58 @@
-import { USER_LS, userForm } from './greeting.js';
-const quotes = document.querySelector('.quotes');
-const QUOTES_LS = 'quotes';
+'use strict';
 
-const onNameSubmit = () => {
-  userForm.addEventListener('submit', getQuotes);
-};
-
-const paintQuotes = (obj) => {
-  const quotesContent = document.createElement('h1');
-  quotesContent.setAttribute('class', 'quotes__content');
-
-  const quotesAuthor = document.createElement('span');
-  quotesAuthor.setAttribute('class', 'quotes__author');
-
-  quotesContent.textContent = `"${obj.quote}"`;
-  quotesAuthor.textContent = `- ${obj.author} -`;
-
-  quotes.appendChild(quotesContent);
-  quotes.appendChild(quotesAuthor);
-};
-
-const getQuotes = () => {
-  const quotesParsedObj = JSON.parse(localStorage.getItem(QUOTES_LS));
-
-  if (quotesParsedObj === null || quotesParsedObj.day !== new Date().getDay()) {
-    fetch('https://type.fit/api/quotes')
-      .then((response) => {
-        return response.json();
-      })
-      .then((json) => {
-        const quotesIndex = Math.floor(Math.random() * json.length);
-        const author =
-          json[quotesIndex].author === null
-            ? 'Author Unknown '
-            : json[quotesIndex].author;
-        const text = json[quotesIndex].text;
-        const day = new Date().getDay();
-
-        const quoteObj = {
-          day: day,
-          quotesIndex: quotesIndex,
-          author: author,
-          quote: text,
-        };
-
-        localStorage.setItem(QUOTES_LS, JSON.stringify(quoteObj));
-
-        paintQuotes(quoteObj);
-      });
-  } else {
-    paintQuotes(quotesParsedObj);
+export default class Quotes {
+  constructor() {
+    this.quotes = document.querySelector('.quotes');
+    this.QUOTES_LS = 'quotes';
   }
-};
 
-const quotesInit = () => {
-  const userName = localStorage.getItem(USER_LS);
-  if (userName !== null) {
-    getQuotes();
-  }
-  onNameSubmit();
-};
+  paintQuotes = (obj) => {
+    const quotesContent = document.createElement('h1');
+    quotesContent.setAttribute('class', 'quotes__content');
 
-quotesInit();
+    const quotesAuthor = document.createElement('span');
+    quotesAuthor.setAttribute('class', 'quotes__author');
+
+    quotesContent.textContent = `"${obj.quote}"`;
+    quotesAuthor.textContent = `- ${obj.author} -`;
+
+    this.quotes.appendChild(quotesContent);
+    this.quotes.appendChild(quotesAuthor);
+  };
+
+  getQuotes = () => {
+    const quotesParsedObj = JSON.parse(localStorage.getItem(this.QUOTES_LS));
+
+    if (
+      quotesParsedObj === null ||
+      quotesParsedObj.day !== new Date().getDay()
+    ) {
+      fetch('https://type.fit/api/quotes')
+        .then((response) => {
+          return response.json();
+        })
+        .then((json) => {
+          const quotesIndex = Math.floor(Math.random() * json.length);
+          const author =
+            json[quotesIndex].author === null
+              ? 'Author Unknown '
+              : json[quotesIndex].author;
+          const text = json[quotesIndex].text;
+          const day = new Date().getDay();
+
+          const quoteObj = {
+            day: day,
+            quotesIndex: quotesIndex,
+            author: author,
+            quote: text,
+          };
+
+          localStorage.setItem(this.QUOTES_LS, JSON.stringify(quoteObj));
+
+          this.paintQuotes(quoteObj);
+        });
+    } else {
+      this.paintQuotes(quotesParsedObj);
+    }
+  };
+}
