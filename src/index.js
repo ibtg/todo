4e1a83ec88727eae669892b25df6ced78b986d98
ibtg <ibtg0409@gmail.com>
2020-09-02 19:02:53 +0900

    Refactor todo functions to class

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import Clock from './clock.js';
 import Weather from './weather.js';
 import Greeting from './greeting.js';
 import Quotes from './quotes.js';
+import Todo from './todo.js';
+
+const SHOWING_ON = 'form__showing';
 
 const clock = new Clock();
 clock.StartTime();
@@ -17,9 +20,14 @@ greeting.loadName();
 greeting.userForm.addEventListener('submit', () => {
   const quotes = new Quotes();
   quotes.getQuotes();
+  const todo = new Todo(SHOWING_ON);
+  todo.loadTodos();
 });
 
 if (localStorage.getItem(greeting.USER_LS)) {
   const quotes = new Quotes();
   quotes.getQuotes();
+
+  const todo = new Todo(SHOWING_ON);
+  todo.loadTodos();
 }
--- src/todo.js
+++ src/todo.js
@@ -1,129 +1,120 @@
-import { USER_LS, userForm, SHOWING_ON } from './greeting.js';
+'user strict';
 
-const todo = document.querySelector('.todo');
-const todoForm = document.querySelector('.todo__form');
-const todoInput = document.querySelector('.todo__input');
-const todoList = document.querySelector('.todo__lists');
+export default class Todo {
+  constructor(showing) {
+    this.todo = document.querySelector('.todo');
+    this.todoForm = document.querySelector('.todo__form');
+    this.todoInput = document.querySelector('.todo__input');
+    this.todoList = document.querySelector('.todo__lists');
+    this.TODOS_LS = 'toDos';
+    this.todos = [];
+    this.showing = showing;
 
-const TODOS_LS = 'toDos';
-let todos = [];
-
-const onNameSubmit = () => {
-  userForm.addEventListener('submit', () => {
-    todo.classList.add(SHOWING_ON);
-  });
-};
-
-const saveTodos = (todos) => {
-  localStorage.setItem(TODOS_LS, JSON.stringify(todos));
-};
-
-const checkBtn = (event) => {
-  const btn = event.target.parentNode;
-  const unchecked = btn.classList.toggle('unchecked');
-  const li = btn.parentNode;
-
-  if (!unchecked) {
-    btn.innerHTML = '<i class="far fa-check-square"></i>';
-    li.style.textDecoration = 'line-through';
-  } else {
-    btn.innerHTML = '<i class="far fa-square"></i>';
-    li.style.textDecoration = 'none';
-  }
-};
-
-const deleteBtn = (event) => {
-  const btn = event.target.parentNode;
-  const li = btn.parentNode;
-
-  todoList.removeChild(li);
-  const cleanTodos = todos.filter((todo) => {
-    return todo.id !== parseInt(li.id);
-  });
-
-  todos = cleanTodos;
-
-  saveTodos(todos);
-};
-
-const paintTodo = (text) => {
-  if (text === '') {
-    todoInput.focus();
-    return;
-  }
-
-  const li = document.createElement('li');
-  li.setAttribute('class', 'todo__item');
-
-  const check = document.createElement('button');
-  check.setAttribute('class', 'todo__check unchecked');
-
-  const span = document.createElement('span');
-  span.setAttribute('class', 'todo__content');
-
-  const delBtn = document.createElement('button');
-  delBtn.setAttribute('class', 'todo__delete');
-
-  const newId = todos.length + 1;
-
-  check.innerHTML = '<i class="far fa-square"></i>';
-  check.addEventListener('click', checkBtn);
-
-  delBtn.innerHTML = '<i class="fas fa-times"></i>';
-  delBtn.addEventListener('click', deleteBtn);
-
-  span.innerText = text;
-
-  li.appendChild(check);
-  li.appendChild(span);
-  li.appendChild(delBtn);
-  li.scrollIntoView({ block: 'center' });
-
-  li.id = newId;
-
-  todoList.appendChild(li);
-  const todoObj = {
-    id: newId,
-    text: text,
-  };
-
-  todos.push(todoObj);
-  saveTodos(todos);
-};
-
-const onHandleSubmit = (event) => {
-  event.preventDefault();
-  if (todos.length < 10) {
-    const currentValue = todoInput.value;
-    paintTodo(currentValue);
-  }
-
-  todoInput.value = '';
-  todoInput.focus();
-};
-
-const loadTodos = () => {
-  const loadedTodos = localStorage.getItem(TODOS_LS);
-  const parsedTodos = JSON.parse(loadedTodos);
-
-  if (loadedTodos !== null) {
-    parsedTodos.forEach((todo) => {
-      paintTodo(todo.text);
+    this.todoForm.addEventListener('submit', (event) => {
+      this.onHandleSubmit(event);
     });
   }
-};
 
-const todoInit = () => {
-  const userName = localStorage.getItem(USER_LS);
+  saveTodos = (todos) => {
+    localStorage.setItem(this.TODOS_LS, JSON.stringify(todos));
+  };
 
-  if (userName !== null) {
-    todo.classList.add(SHOWING_ON);
-  }
-  loadTodos();
+  checkBtn = (event) => {
+    const btn = event.target.parentNode;
+    const unchecked = btn.classList.toggle('unchecked');
+    const li = btn.parentNode;
 
-  todoForm.addEventListener('submit', onHandleSubmit);
-  todoInput.focus();
-  onNameSubmit();
-};
+    if (!unchecked) {
+      btn.innerHTML = '<i class="far fa-check-square"></i>';
+      li.style.textDecoration = 'line-through';
+    } else {
+      btn.innerHTML = '<i class="far fa-square"></i>';
+      li.style.textDecoration = 'none';
+    }
+  };
 
-todoInit();
+  deleteBtn = (event) => {
+    const btn = event.target.parentNode;
+    const li = btn.parentNode;
+
+    this.todoList.removeChild(li);
+    const cleanTodos = this.todos.filter((todo) => {
+      return todo.id !== parseInt(li.id);
+    });
+
+    this.todos = cleanTodos;
+    this.saveTodos(this.todos);
+  };
+
+  paintTodo = (text) => {
+    if (text === '') {
+      this.todoInput.focus();
+      return;
+    }
+
+    const li = document.createElement('li');
+    li.setAttribute('class', 'todo__item');
+
+    const check = document.createElement('button');
+    check.setAttribute('class', 'todo__check unchecked');
+
+    const span = document.createElement('span');
+    span.setAttribute('class', 'todo__content');
+
+    const delBtn = document.createElement('button');
+    delBtn.setAttribute('class', 'todo__delete');
+
+    const newId = this.todos.length + 1;
+
+    check.innerHTML = '<i class="far fa-square"></i>';
+    check.addEventListener('click', this.checkBtn);
+
+    delBtn.innerHTML = '<i class="fas fa-times"></i>';
+    delBtn.addEventListener('click', this.deleteBtn);
+
+    span.innerText = text;
+
+    li.appendChild(check);
+    li.appendChild(span);
+    li.appendChild(delBtn);
+    li.scrollIntoView({ block: 'center' });
+
+    li.id = newId;
+
+    this.todoList.appendChild(li);
+    const todoObj = {
+      id: newId,
+      text: text,
+    };
+
+    this.todos.push(todoObj);
+    this.saveTodos(this.todos);
+  };
+
+  onHandleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log('submit: ', event);
+    if (this.todos.length < 10) {
+      const currentValue = this.todoInput.value;
+      this.paintTodo(currentValue);
+    }
+
+    this.todoInput.value = '';
+    this.todoInput.focus();
+  };
+
+  loadTodos = () => {
+    this.todoForm.classList.add(this.showing);
+    this.todo.classList.add(this.showing);
+    const loadedTodos = localStorage.getItem(this.TODOS_LS);
+    const parsedTodos = JSON.parse(loadedTodos);
+
+    if (loadedTodos !== null) {
+      parsedTodos.forEach((todo) => {
+        this.paintTodo(todo.text);
+      });
+    }
+    this.todoInput.focus();
+  };
+}
