d53867b12414828d12202a3fe2fd9345271c5ec8
ibtg <ibtg0409@gmail.com>
2020-11-01 19:20:45 +0900

    Add responsive web

--- src/index.js
+++ src/index.js
@@ -2,15 +2,10 @@
 
 import Clock from './clock.js';
 import Weather from './weather.js';
-import Greeting from './greeting.js';
 import Quotes from './quotes.js';
 import Todo from './todo.js';
 import Days from './days.js';
-// import Background from './background.js';
 
-const SHOWING_ON = 'form__showing';
-const GOAL = 'goal';
-const DOING = 'doing'
 
 const clock = new Clock();
 clock.StartTime();
@@ -23,30 +18,6 @@ quotes.getQuotes();
 
 const days = new Days();
 days.getDays();
-
-// const greeting = new Greeting();
-// greeting.loadName();
-
-// const background = new Background();
-// background.loadBackground();
-
-// greeting.userForm.addEventListener('submit', () => {
-
-
-//   const goal = new Todo(SHOWING_ON, 'GOAL');
-//   const doing = new Todo(SHOWING_ON, 'DOING');
-//   const todo = new Todo(SHOWING_ON, 'TODO');
-//   const done = new Todo(SHOWING_ON, 'DONE');
-
-//   goal.loadTodos();
-//   doing.loadTodos();
-//   todo.loadTodos();
-//   done.loadTodos();
-
-
-// });
-// const quotes = new Quotes();
-// quotes.getQuotes();
 
 const goal = new Todo('.item__goal');
 const doing = new Todo('.item__doing');
